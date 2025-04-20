da638c2c3aaa0029f13b37dfd16b22f88f9119f4
Yasin <yasinpeker43@gmail.com>
2025-04-20 21:56:49 +0300

    Added some styles to NewProject.jsx.

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -2,10 +2,10 @@ import Input from "./Input.jsx";
 
 export default function NewProject() {
     return (
-      <div>
-        <menu>
-            <button>Cancel</button>
-            <button>Save</button>
+      <div className="w-[35rem] mt-16">
+        <menu className="flex item-center justify-end gap-4 my-1">
+            <button className="text-stone-800 hover:text-stone-950">Cancel</button>
+            <button className="rounded-md bg-stone-950 text-stone-50 hover:bg-stone-600 px-4 py-1">Save</button>
         </menu>
         <div>
              <Input label="Title" />
