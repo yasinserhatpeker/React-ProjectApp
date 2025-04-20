4f85a6fd8b6bbaa0b1aee78245d38d2831c5bb23
Yasin <yasinpeker43@gmail.com>
2025-04-20 21:39:24 +0300

    NewProject.jsx is created.

---
+++ src/components/NewProject.jsx
@@ -0,0 +1,24 @@
+export default function NewProject() {
+    return (
+      <div>
+        <menu>
+            <button>Cancel</button>
+            <button>Save</button>
+        </menu>
+        <div>
+             <p>
+                <label>Title</label>
+                <input  />
+             </p>
+             <p>
+                <label>Description</label>
+                <textarea ></textarea>
+             </p>
+             <p>
+                <label>Due Date</label>
+                <input  />
+             </p>
+        </div>
+      </div>
+    )
+}
