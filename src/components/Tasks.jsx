8302866ae5f6e12b873bc8b9871417f0a81bbb2d
Yasin <yasinpeker43@gmail.com>
2025-04-23 10:37:57 +0300

    NewTasks component is created.

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,8 +1,10 @@
+import NewTasks from "./NewTasks.jsx";
+
 export default function Tasks() {
     return (
         <section>
             <h2 className="text-2xl font-bold text-stone-700 mb-4">Tasks</h2>
-            NEW TASKS
+            <NewTasks/>
             <p className="text-stone-800 mb-4">This project does not have any tasks yet.</p>
             <ul>
 
